9887621d2cec6fee389d4a8159344f651471504a
Ghydaa Abu-zainh <ghydaa.abuzainh3@gmail.com>
2024-01-26 04:33:01 +0300

    last update on items

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,38 +1,72 @@
 /// <reference types= "cypress" />
 
+Cypress.Commands.add( 'toAllText' , () => {
+
+  let randomitems=Math.floor(Math.random()*4)
+    cy.get('.product-items').find('.product-item-info').eq(randomitems).click()
+    let randomsize=Math.floor(Math.random()*3)
+
+      cy.get('.swatch-attribute-options').find('.swatch-option').eq(randomsize).click()
+      let randomcolor=Math.floor(Math.random()*3)
+
+      cy.get('.swatch-attribute.color > .swatch-attribute-options').find('.swatch-option').eq(randomcolor).click()
+    cy.get('.stock > span').invoke('text').then((thetext) => {
+      if (thetext=='In stock') {
+       cy.get('#product-addtocart-button').click()
+        
+      }
+      else {
+        alert('sorry this item is not there ')
+      }
+      
+      cy.wait(5000) 
+
+    })
+
+
+})
+
 describe('Magento test case ', () => {
-  it('test on some items', () => {
+  it('add random item from Women category', () => {
 
     cy.visit('https://magento.softwaretestingboard.com/')
 
-    let mylist=['Women' , 'Gear']
-    let randomlist=Math.floor(Math.random()*mylist.length)
-    cy.contains(mylist[randomlist]).click()
 
-    let randomitems=Math.floor(Math.random()*4)
+    
+    cy.get('#ui-id-4').click()
+    
+    
+    cy.toAllText()
+
+    cy.wait(5000)
+   
+    }
+    
+    );
+
+
+    it('add random item from Men category', () => {
+
+      cy.visit('https://magento.softwaretestingboard.com/')
+
+    
+    cy.get('#ui-id-5').click()
+    
+    cy.toAllText()
+
+    cy.wait(5000) 
+    });
+
+    it('add random item from Gear category', () => {
+      cy.visit('https://magento.softwaretestingboard.com/')
+
+    
+      cy.get('#ui-id-6').click()
+      let randomitems=Math.floor(Math.random()*4)
     cy.get('.product-items').find('.product-item-info').eq(randomitems).click()
 
-    let randomsize=Math.floor(Math.random()*3)
-    let randomcolor=Math.floor(Math.random()*3)
-    if(randomlist==0){
-      cy.get('.swatch-attribute-options').find('.swatch-option').eq(randomsize).click()
-      cy.get('.swatch-attribute.color > .swatch-attribute-options').find('.swatch-option').eq(randomcolor).click()
-      cy.get('.stock > span').invoke('text').then((thetext) => {
-        if (thetext=='In stock') {
-         cy.get('#product-addtocart-button').click()
-          
-        }
-        else {
-          alert('sorry this item is not there ')
-        }
-        
-      })
-
-    }
-    
-    else {
       cy.get('#product-addtocart-button').click()
-    }
-    
-  });
+
+      cy.wait(5000) 
+    });
 });
